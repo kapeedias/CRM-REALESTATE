657244fc09cc41213246858bd91c1af206bd51e7
Sai Deepak <sai@livewd.ca>
2023-12-01 05:12:56 +0530

    sizemod

--- login.php
+++ login.php
@@ -75,11 +75,11 @@ if (Input::exists()) {
             <div class="page-content d-flex align-items-center justify-content-center">
 
                 <div class="row w-100 mx-0 auth-page">
-                    <div class="col-md-8 col-xl-6 mx-auto">
+                    <div class="col-md-6 col-xl-6 mx-auto">
                         <div class="card">
                             <div class="row">
                                 
-                                <div class="col-md-4 ps-md-0">
+                                <div class="col-md-6 ps-md-0">
                                     <div class="auth-form-wrapper px-4 py-5">
                                         <a href="#" class="noble-ui-logo d-block mb-2">REAL<span>Engine</span></a>
                                         <h5 class="text-muted fw-normal mb-4">Welcome back! Log in to your account.</h5>
