c3027069a030de3983c84b41040b16214df58866
Sai Deepak <sai@livewd.ca>
2023-12-01 04:41:54 +0530

    index

--- login.php
+++ login.php
@@ -3,31 +3,31 @@
 require_once 'core/init.php';
 
 if (Input::exists()) {
-  if (Token::check(Input::get('token'))) {
-    $validate = new Validate();
-    $validation = $validate->check(
-      array(
-        'username' => array('required' => true),
-        'password' => array('required' => true),
-      )
-    );
+    if (Token::check(Input::get('token'))) {
+        $validate = new Validate();
+        $validation = $validate->check(
+            array(
+                'username' => array('required' => true),
+                'password' => array('required' => true),
+            )
+        );
 
-    if ($validation->passed()) {
-      $user = new User();
-      $remember = (Input::get('remember') === 'on') ? true : false;
-      $login = $user->login(Input::get('username'), Input::get('password'), $remember);
+        if ($validation->passed()) {
+            $user = new User();
+            $remember = (Input::get('remember') === 'on') ? true : false;
+            $login = $user->login(Input::get('username'), Input::get('password'), $remember);
 
-      if ($login) {
-        echo 'Success';
-      } else {
-        echo 'Login failed';
-      }
-    } else {
-      foreach ($validation->errors() as $error) {
-        echo $error . '<br>';
-      }
+            if ($login) {
+                echo 'Success';
+            } else {
+                echo 'Login failed';
+            }
+        } else {
+            foreach ($validation->errors() as $error) {
+                echo $error . '<br>';
+            }
+        }
     }
-  }
 }
 
 ?>
@@ -36,101 +36,102 @@ if (Input::exists()) {
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <meta name="description" content="A Powerful CRM for Realtors">
-  <meta name="author" content="Live Web Design">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="A Powerful CRM for Realtors">
+    <meta name="author" content="Live Web Design">
 
-  <title>RealEngine - A CRM built for Realtors</title>
+    <title>RealEngine - A CRM built for Realtors</title>
 
-  <!-- Fonts -->
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
-  <!-- End fonts -->
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+    <!-- End fonts -->
 
-  <!-- core:css -->
-  <link rel="stylesheet" href="assets/vendors/core/core.css">
-  <!-- endinject -->
+    <!-- core:css -->
+    <link rel="stylesheet" href="assets/vendors/core/core.css">
+    <!-- endinject -->
 
-  <!-- Plugin css for this page -->
-  <!-- End plugin css for this page -->
+    <!-- Plugin css for this page -->
+    <!-- End plugin css for this page -->
 
-  <!-- inject:css -->
-  <link rel="stylesheet" href="assets/fonts/feather-font/css/iconfont.css">
-  <link rel="stylesheet" href="assets/vendors/flag-icon-css/css/flag-icon.min.css">
-  <!-- endinject -->
+    <!-- inject:css -->
+    <link rel="stylesheet" href="assets/fonts/feather-font/css/iconfont.css">
+    <link rel="stylesheet" href="assets/vendors/flag-icon-css/css/flag-icon.min.css">
+    <!-- endinject -->
 
-  <!-- Layout styles -->
-  <link rel="stylesheet" href="assets/css/style.css">
-  <!-- End layout styles -->
+    <!-- Layout styles -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <!-- End layout styles -->
 
-  <link rel="shortcut icon" href="assets/images/favicon.png" />
+    <link rel="shortcut icon" href="assets/images/favicon.png" />
 </head>
 
 <body>
-  <div class="main-wrapper">
-    <div class="page-wrapper full-page">
-      <div class="page-content d-flex align-items-center justify-content-center">
+    <div class="main-wrapper">
+        <div class="page-wrapper full-page">
+            <div class="page-content d-flex align-items-center justify-content-center">
 
-        <div class="row w-100 mx-0 auth-page">
-          <div class="col-md-8 col-xl-6 mx-auto">
-            <div class="card">
-              <div class="row">
-                <div class="col-md-4 pe-md-0">
-                  <div class="auth-side-wrapper">
+                <div class="row w-100 mx-0 auth-page">
+                    <div class="col-md-8 col-xl-6 mx-auto">
+                        <div class="card">
+                            <div class="row">
+                                <div class="col-md-4 pe-md-0">
+                                    <div class="auth-side-wrapper">
 
-                  </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-8 ps-md-0">
+                                    <div class="auth-form-wrapper px-4 py-5">
+                                        <a href="#" class="noble-ui-logo d-block mb-2">REAL<span>Engine</span></a>
+                                        <h5 class="text-muted fw-normal mb-4">Welcome back! Log in to your account.</h5>
+                                        <form action="" method="POST" class="form">
+                                            <div class="mb-3">
+                                                <label for="userEmail" class="form-label">Email address</label>
+                                                <input type="text" name="username" id="username" value="<?php echo Input::get('username'); ?>" class="form-control" autocomplete="off" placeholder="Email" />
+                                            </div>
+                                            <div class="mb-3">
+                                                <label for="userPassword" class="form-label">Password</label>
+                                                <input type="password" name="password" id="password" class="form-control" autocomplete="off" />
+                                            </div>
+                                            <div class="form-check mb-3">
+                                                <input type="checkbox" name="remember" id="remember" class="form-control" />
+                                                <label class="form-check-label" for="authCheck">
+                                                    Remember me
+                                                </label>
+                                            </div>
+                                            <div class="form-check mb-3">
+                                                <input type="hidden" name="token" value="<?php echo Token::generate(); ?>" />
+                                                <input type="submit" name="doLogin" id="doLogin" value="Login" class="btn btn-success" />
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-md-8 ps-md-0">
-                  <div class="auth-form-wrapper px-4 py-5">
-                    <a href="#" class="noble-ui-logo d-block mb-2">REAL<span>Engine</span></a>
-                    <h5 class="text-muted fw-normal mb-4">Welcome back! Log in to your account.</h5>
-                    <form action="" method="POST" class="form">
-                      <div class="mb-3">
-                        <label for="userEmail" class="form-label">Email address</label>
-                        <input type="text" name="username" id="username" value="<?php echo Input::get('username'); ?>" class="form-control" autocomplete="off" placeholder="Email" />
-                      </div>
-                      <div class="mb-3">
-                        <label for="userPassword" class="form-label">Password</label>
-                        <input type="password" name="password" id="password" class="form-control" autocomplete="off" />
-                      </div>
-                      <div class="form-check mb-3">
-                        <label class="form-check-label" for="authCheck">
-                          <input type="checkbox" name="remember" id="remember" class="form-control" />Remember me
-                        </label>
-                        <input type="hidden" name="token" value="<?php echo Token::generate(); ?>" />
-                        <input type="submit" name="doLogin" id="doLogin" value="Login" class="btn btn-success" />
 
-                      </div>
-                      
-                    </form>
-                  </div>
-                </div>
-              </div>
             </div>
-          </div>
         </div>
-
-      </div>
     </div>
-  </div>
 
-  <!-- core:js -->
-  <script src="assets/vendors/core/core.js"></script>
-  <!-- endinject -->
+    <!-- core:js -->
+    <script src="assets/vendors/core/core.js"></script>
+    <!-- endinject -->
 
-  <!-- Plugin js for this page -->
-  <!-- End plugin js for this page -->
+    <!-- Plugin js for this page -->
+    <!-- End plugin js for this page -->
 
-  <!-- inject:js -->
-  <script src="assets/vendors/feather-icons/feather.min.js"></script>
-  <script src="assets/js/template.js"></script>
-  <!-- endinject -->
+    <!-- inject:js -->
+    <script src="assets/vendors/feather-icons/feather.min.js"></script>
+    <script src="assets/js/template.js"></script>
+    <!-- endinject -->
 
-  <!-- Custom js for this page -->
-  <!-- End custom js for this page -->
+    <!-- Custom js for this page -->
+    <!-- End custom js for this page -->
 
 </body>
 
