0d32bd28e3cddf68c8005482a59f272631eec33f
Sai Deepak <sai@livewd.ca>
2023-12-01 05:11:03 +0530

    loginform

--- index.php
+++ index.php
@@ -78,11 +78,7 @@ if (Input::exists()) {
                     <div class="col-md-8 col-xl-6 mx-auto">
                         <div class="card">
                             <div class="row">
-                                <div class="col-md-4 pe-md-0">
-                                    <div class="auth-side-wrapper">
-
-                                    </div>
-                                </div>
+                                
                                 <div class="col-md-8 ps-md-0">
                                     <div class="auth-form-wrapper px-4 py-5">
                                         <a href="#" class="noble-ui-logo d-block mb-2">REAL<span>Engine</span></a>
