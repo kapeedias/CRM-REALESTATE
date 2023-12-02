235d19371c63473d625e4c418de62fb96c8935c9
Sai Deepak <sai@livewd.ca>
2023-12-02 10:56:39 +0530

    AA

--- classes/User.php
+++ classes/User.php
@@ -1,45 +1,45 @@
 <?php
 
-class User{
-    private $_db
-            ,$_data
-            ,$_sessionName
-            ,$_cookieName
-            ,$_isLoggedIn;
+class User
+{
+    private $_db, $_data, $_sessionName, $_cookieName, $_isLoggedIn;
 
 
-    public function __construct($user = null){
+    public function __construct($user = null)
+    {
         $this->_db = DB::getInstance();
-       $this->_sessionName = Config::get('session/session_name');
+        $this->_sessionName = Config::get('session/session_name');
         $this->_cookieName = Config::get('remember/cookie_name');
-        
-        if(!$user){
-            if(Session::exists($this->_sessionName)){
+
+        if (!$user) {
+            if (Session::exists($this->_sessionName)) {
                 $user = Session::get($this->_sessionName);
-                if($this->find($user)){
+                if ($this->find($user)) {
                     $this->_isLoggedIn = true;
-                }else{
+                } else {
                     //process logout
                 }
             }
-        }else{
+        } else {
             $this->find($user);
-        } 
+        }
     }
 
-    public function create($fields = array()){
-        if($this->_db->insert('crm_users',$fields)){
+    public function create($fields = array())
+    {
+        if ($this->_db->insert('crm_users', $fields)) {
             throw new Exception('There was a problem creating your account');
         }
     }
 
-    public function find($user = null){
-    
-        if($user){
-            $field = (is_numeric($user)) ? 'id' : 'username';
-            $data = $this->_db->get('crm_users',array($field, '=', $user));
+    public function find($user = null)
+    {
 
-            if($data->count()){
+        if ($user) {
+            $field = (is_numeric($user)) ? 'id' : 'username';
+            $data = $this->_db->get('crm_users', array($field, '=', $user));
+
+            if ($data->count()) {
                 $this->_data = $data->first();
                 return true;
             }
@@ -47,18 +47,20 @@ class User{
         return false;
     }
 
-    public function login($username = null, $password = null, $remember=false){
+    public function login($username = null, $password = null, $remember = false)
+    {
         $user = $this->find($username);
 
-        if($user){
-            if($this->data()->password === Hash::make($password, $this->data()->salt)){
-               Session::put($this->_sessionName, $this->data()->id);
+        if ($user) {
+            //if($this->data()->password === Hash::make($password, $this->data()->salt)){
+            if (isset($this->data()->password) && Hash::make($password, $this->data()->salt) === $this->data()->password) {
+                Session::put($this->_sessionName, $this->data()->id);
 
-                if($remember){
+                if ($remember) {
                     $hash = Hash::unique();
                     $hashCheck = $this->_db->get('users_session', array('user_id', '=', $this->data()->id));
-                    if(!$hashCheck->count()){
-                        $this->_db->insert('users_session',array(
+                    if (!$hashCheck->count()) {
+                        $this->_db->insert('users_session', array(
                             'user_id' => $this->data()->id,
                             'hash' => $hash,
                             'ip' => $_SERVER['REMOTE_ADDR']
@@ -66,27 +68,31 @@ class User{
                     } else {
                         $hash = $hashCheck->first()->hash;
                     }
-                
-                    Cookie::put($this->_cookieName, $hash, Config::get('remember/cookie_expiry'));
 
+                    Cookie::put($this->_cookieName, $hash, Config::get('remember/cookie_expiry'));
                 }
 
 
-               return true;
+                return true;
+            } else {
+                echo "Password doesn't exists error";
             }
         }
         return false;
     }
 
-    public function logout(){
+    public function logout()
+    {
         Session::delete($this->_sessionName);
     }
 
-    public function data(){
+    public function data()
+    {
         return $this->_data;
     }
 
-    public function isLoggedIn(){
+    public function isLoggedIn()
+    {
         return $this->_isLoggedIn;
     }
 }
