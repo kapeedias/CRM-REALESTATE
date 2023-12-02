54ff57e02e4be2a93c5700c1a191485d0b5011c3
Sai Deepak <sai@livewd.ca>
2023-12-02 06:25:36 +0530

    db

--- classes/DB.php
+++ classes/DB.php
@@ -80,6 +80,9 @@ class DB
 
     public function insert($table, $fields = array())
     {
+        if (!is_array($fields)) {
+            $fields = array('value' => $fields);
+        }
 
         $keys = array_keys($fields);
         $values = '';
