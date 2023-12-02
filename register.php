40af2ea1944748720a0c2022fd73cd994613b998
Sai Deepak <sai@livewd.ca>
2023-12-02 07:40:30 +0530

    !!

--- register.php
+++ register.php
@@ -146,7 +146,7 @@ if (Input::exists()) {
                                                 <input type="password" name="password" id="password" value="" class="form-control" autocomplete="off" required />
                                             </div>
                                             <div class="mb-3">
-                                                <label for="passwordagain" class="form-label">Re-Enter Password</label>
+                                                <label for="password_again" class="form-label">Re-Enter Password</label>
                                                 <input type="password" name="password_again" id="password_again" value="" class="form-control" autocomplete="off" required />
                                             </div>
                                             <div>
