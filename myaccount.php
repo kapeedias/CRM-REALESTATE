5a9279985198eee643757c1e53e423d067c8e06a
Sai Deepak <sai@livewd.ca>
2023-12-04 04:24:43 +0530

    aas133

--- myaccount.php
+++ myaccount.php
@@ -385,12 +385,42 @@ if(!$user->isLoggedIn()){
          
           <ul class="navbar-nav">
             <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="languageDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="flag-icon flag-icon-us mt-1" title="us"></i> <span class="ms-1 me-1 d-none d-md-inline-block">English</span>
+              </a>
+              <div class="dropdown-menu" aria-labelledby="languageDropdown">
+                <a href="javascript:;" class="dropdown-item py-2"><i class="flag-icon flag-icon-us" title="us" id="us"></i> <span class="ms-1"> English </span></a>
+                <a href="javascript:;" class="dropdown-item py-2"><i class="flag-icon flag-icon-fr" title="fr" id="fr"></i> <span class="ms-1"> French </span></a>
+                <a href="javascript:;" class="dropdown-item py-2"><i class="flag-icon flag-icon-de" title="de" id="de"></i> <span class="ms-1"> German </span></a>
+                <a href="javascript:;" class="dropdown-item py-2"><i class="flag-icon flag-icon-pt" title="pt" id="pt"></i> <span class="ms-1"> Portuguese </span></a>
+                <a href="javascript:;" class="dropdown-item py-2"><i class="flag-icon flag-icon-es" title="es" id="es"></i> <span class="ms-1"> Spanish </span></a>
+              </div>
+            </li>
+            <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="#" id="appsDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <i data-feather="grid"></i>
               </a>
               <div class="dropdown-menu p-0" aria-labelledby="appsDropdown">
-                
-                  
+                <div class="px-3 py-2 d-flex align-items-center justify-content-between border-bottom">
+                  <p class="mb-0 fw-bold">Web Apps</p>
+                  <a href="javascript:;" class="text-muted">Edit</a>
+                </div>
+                <div class="row g-0 p-1">
+                  <div class="col-3 text-center">
+                    <a href="pages/apps/chat.html" class="dropdown-item d-flex flex-column align-items-center justify-content-center wd-70 ht-70"><i data-feather="message-square" class="icon-lg mb-1"></i>
+                      <p class="tx-12">Chat</p>
+                    </a>
+                  </div>
+                  <div class="col-3 text-center">
+                    <a href="pages/apps/calendar.html" class="dropdown-item d-flex flex-column align-items-center justify-content-center wd-70 ht-70"><i data-feather="calendar" class="icon-lg mb-1"></i>
+                      <p class="tx-12">Calendar</p>
+                    </a>
+                  </div>
+                  <div class="col-3 text-center">
+                    <a href="pages/email/inbox.html" class="dropdown-item d-flex flex-column align-items-center justify-content-center wd-70 ht-70"><i data-feather="mail" class="icon-lg mb-1"></i>
+                      <p class="tx-12">Email</p>
+                    </a>
+                  </div>
                   <div class="col-3 text-center">
                     <a href="pages/general/profile.html" class="dropdown-item d-flex flex-column align-items-center justify-content-center wd-70 ht-70"><i data-feather="instagram" class="icon-lg mb-1"></i>
                       <p class="tx-12">Profile</p>
