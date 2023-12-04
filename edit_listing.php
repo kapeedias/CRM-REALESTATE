0ba214b820e8f8301e72a825758ac896cf5b6231
Sai Deepak <sai@livewd.ca>
2023-12-04 14:11:30 +0530

    as

--- edit_listing.php
+++ edit_listing.php
@@ -149,34 +149,35 @@ try {
 
                         <div class="row">
                             <div class="col-md-6">
-                                <div class="card-body">
+                                <div class="card">
+                                    <div class="card-body">
 
-                                    <h6 class="card-title">Basic Form</h6>
+                                        <h6 class="card-title">Basic Form</h6>
 
-                                    <form class="forms-sample">
-                                        <div class="mb-3">
-                                            <label for="exampleInputUsername1" class="form-label">Username</label>
-                                            <input type="text" class="form-control" id="exampleInputUsername1" autocomplete="off" placeholder="Username">
-                                        </div>
-                                        <div class="mb-3">
-                                            <label for="exampleInputEmail1" class="form-label">Email address</label>
-                                            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
-                                        </div>
-                                        <div class="mb-3">
-                                            <label for="exampleInputPassword1" class="form-label">Password</label>
-                                            <input type="password" class="form-control" id="exampleInputPassword1" autocomplete="off" placeholder="Password">
-                                        </div>
-                                        <div class="form-check mb-3">
-                                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                            <label class="form-check-label" for="exampleCheck1">
-                                                Remember me
-                                            </label>
-                                        </div>
-                                        <button type="submit" class="btn btn-primary me-2">Submit</button>
-                                        <button class="btn btn-secondary">Cancel</button>
-                                    </form>
+                                        <form class="forms-sample">
+                                            <div class="mb-3">
+                                                <label for="exampleInputUsername1" class="form-label">Username</label>
+                                                <input type="text" class="form-control" id="exampleInputUsername1" autocomplete="off" placeholder="Username">
+                                            </div>
+                                            <div class="mb-3">
+                                                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                                                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+                                            </div>
+                                            <div class="mb-3">
+                                                <label for="exampleInputPassword1" class="form-label">Password</label>
+                                                <input type="password" class="form-control" id="exampleInputPassword1" autocomplete="off" placeholder="Password">
+                                            </div>
+                                            <div class="form-check mb-3">
+                                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                                <label class="form-check-label" for="exampleCheck1">
+                                                    Remember me
+                                                </label>
+                                            </div>
+                                            <button type="submit" class="btn btn-primary me-2">Submit</button>
+                                            <button class="btn btn-secondary">Cancel</button>
+                                        </form>
+                                    </div>
                                 </div>
-
                             </div>
 
                         </div>
