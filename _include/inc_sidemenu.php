3bce6e71ae01737e9169fbfc29ad2d3876002a25
Sai Deepak <sai@livewd.ca>
2023-12-04 11:48:36 +0530

    A

--- _include/inc_sidemenu.php
+++ _include/inc_sidemenu.php
@@ -20,7 +20,7 @@
     </li>
     <li class="nav-item">
         <a href="view_inactive_listings.php" class="nav-link">
-            <i class="link-icon" data-feather="message-square"></i>
+            <i class="link-icon" data-feather="triangle"></i>
             <span class="link-title">View Inactive Listings</span>
         </a>
     </li>
