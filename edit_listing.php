402c22d053e39afde48afaa330df0916558ee979
Sai Deepak <sai@livewd.ca>
2023-12-04 14:06:24 +0530

    asdf

--- edit_listing.php
+++ edit_listing.php
@@ -4,12 +4,37 @@ require_once 'classes/Listings.php';
 
 $user = new User();
 if (!$user->isLoggedIn()) {
-  Redirect::to('login.php');
-  die();
+    Redirect::to('login.php');
+    die();
 }
 
-$listing = new Listing();
+$lid = $_GET['id'];
 
+// Replace these variables with your database credentials
+$servername = "localhost";
+$username = "cms_admin";
+$password = "cQ&cH_k)Xybr";
+$dbname = "cms_livewd";
+
+try {
+    // Create a PDO connection
+    $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+    // Set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Your SQL query
+    $sql = "SELECT * FROM listings WHERE id = '$lid'";
+
+    // Prepare and execute the query
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+
+    // Fetch all rows as an associative array
+    $listings = $stmt->fetchAll(PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
 
 
 
@@ -19,140 +44,218 @@ $listing = new Listing();
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <meta name="description" content="A Powerful CRM for REALtors">
-  <meta name="author" content="Live Web Design">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="A Powerful CRM for REALtors">
+    <meta name="author" content="Live Web Design">
 
 
-  <title>REALENGINE - A CRM built for REALtors</title>
+    <title>REALENGINE - A CRM built for REALtors</title>
 
-  <!-- Fonts -->
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
-  <!-- End fonts -->
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+    <!-- End fonts -->
 
-  <!-- core:css -->
-  <link rel="stylesheet" href="assets/vendors/core/core.css">
-  <!-- endinject -->
+    <!-- core:css -->
+    <link rel="stylesheet" href="assets/vendors/core/core.css">
+    <!-- endinject -->
 
-  <!-- Plugin css for this page -->
-  <link rel="stylesheet" href="assets/vendors/flatpickr/flatpickr.min.css">
-  <!-- End plugin css for this page -->
+    <!-- Plugin css for this page -->
+    <link rel="stylesheet" href="assets/vendors/flatpickr/flatpickr.min.css">
+    <!-- End plugin css for this page -->
 
-  <!-- inject:css -->
-  <link rel="stylesheet" href="assets/fonts/feather-font/css/iconfont.css">
-  <link rel="stylesheet" href="assets/vendors/flag-icon-css/css/flag-icon.min.css">
-  <!-- endinject -->
+    <!-- inject:css -->
+    <link rel="stylesheet" href="assets/fonts/feather-font/css/iconfont.css">
+    <link rel="stylesheet" href="assets/vendors/flag-icon-css/css/flag-icon.min.css">
+    <!-- endinject -->
 
-  <!-- Layout styles -->
-  <link rel="stylesheet" href="assets/css/style.css">
-  <!-- End layout styles -->
+    <!-- Layout styles -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <!-- End layout styles -->
 
-  <link rel="shortcut icon" href="assets/images/favicon.png" />
+    <link rel="shortcut icon" href="assets/images/favicon.png" />
 </head>
 
 <body>
-  <div class="main-wrapper">
+    <div class="main-wrapper">
 
-    <!-- partial:partials/_sidebar.html -->
-    <nav class="sidebar">
-      <div class="sidebar-header">
-        <a href="myaccount.php" class="sidebar-brand">
-          REAL<span> ENGINE</span>
-        </a>
-        <div class="sidebar-toggler not-active">
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-      </div>
-      <div class="sidebar-body">
-        <?php include('_include/inc_sidemenu.php'); ?>
-      </div>
-    </nav>
-
-    <!-- partial -->
-
-    <div class="page-wrapper">
-
-      <!-- Top Nav Bar Start -->
-      <nav class="navbar">
-        <a href="#" class="sidebar-toggler">
-          <i data-feather="menu"></i>
-        </a>
-        <div class="navbar-content">
-          <ul class="navbar-nav">
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="profileDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <img class="wd-30 ht-30 rounded-circle" src="https://via.placeholder.com/30x30?text=" alt="profile">
-              </a>
-              <div class="dropdown-menu p-0" aria-labelledby="profileDropdown">
-                <div class="d-flex flex-column align-items-center border-bottom px-5 py-3">
-                  <div class="mb-3">
-                    <img class="wd-80 ht-80 rounded-circle" src="https://via.placeholder.com/80x80?text=" alt="">
-                  </div>
-                  <div class="text-center">
-                    <p class="tx-16 fw-bolder"><?php echo $user->data()->first_name; ?></p>
-                    <p class="tx-12 text-muted"><?php echo $user->data()->email; ?></p>
-                  </div>
+        <!-- partial:partials/_sidebar.html -->
+        <nav class="sidebar">
+            <div class="sidebar-header">
+                <a href="myaccount.php" class="sidebar-brand">
+                    REAL<span> ENGINE</span>
+                </a>
+                <div class="sidebar-toggler not-active">
+                    <span></span>
+                    <span></span>
+                    <span></span>
                 </div>
-                <ul class="list-unstyled p-1">
-                  <li class="dropdown-item py-2">
-                    <a href="logout.php" class="text-body ms-0">
-                      <i class="me-2 icon-md" data-feather="log-out"></i>
-                      <span>Log Out</span>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </nav>
-      <!-- Top Nav Bar End -->
+            </div>
+            <div class="sidebar-body">
+                <?php include('_include/inc_sidemenu.php'); ?>
+            </div>
+        </nav>
 
-      <div class="page-content">
+        <!-- partial -->
 
-        <div class="d-flex justify-content-between align-items-center flex-wrap grid-margin">
-          <div>
-            <h4 class="mb-3 mb-md-0">MLSID: <?php echo $listing->data()->mlsid; ?></h4>
+        <div class="page-wrapper">
+
+            <!-- Top Nav Bar Start -->
+            <nav class="navbar">
+                <a href="#" class="sidebar-toggler">
+                    <i data-feather="menu"></i>
+                </a>
+                <div class="navbar-content">
+                    <ul class="navbar-nav">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="profileDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <img class="wd-30 ht-30 rounded-circle" src="https://via.placeholder.com/30x30?text=" alt="profile">
+                            </a>
+                            <div class="dropdown-menu p-0" aria-labelledby="profileDropdown">
+                                <div class="d-flex flex-column align-items-center border-bottom px-5 py-3">
+                                    <div class="mb-3">
+                                        <img class="wd-80 ht-80 rounded-circle" src="https://via.placeholder.com/80x80?text=" alt="">
+                                    </div>
+                                    <div class="text-center">
+                                        <p class="tx-16 fw-bolder"><?php echo $user->data()->first_name; ?></p>
+                                        <p class="tx-12 text-muted"><?php echo $user->data()->email; ?></p>
+                                    </div>
+                                </div>
+                                <ul class="list-unstyled p-1">
+                                    <li class="dropdown-item py-2">
+                                        <a href="logout.php" class="text-body ms-0">
+                                            <i class="me-2 icon-md" data-feather="log-out"></i>
+                                            <span>Log Out</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <!-- Top Nav Bar End -->
+            <?php if (!empty($listings)) : ?>
+                <div class="page-content">
+
+                    <div class="d-flex justify-content-between align-items-center flex-wrap grid-margin">
+                        <div>
+                            <h4 class="mb-3 mb-md-0">MLSID: <?php echo $listing->data()->mlsid; ?></h4>
+                        </div>
+                    </div>
+                    <?php foreach ($listings as $listing) : ?>
+
+                        <div class="row">
+  <div class="col-md-6 grid-margin stretch-card">
+    <div class="card">
+      <div class="card-body">
+
+        <h6 class="card-title">Basic Form</h6>
+
+        <form class="forms-sample">
+          <div class="mb-3">
+            <label for="exampleInputUsername1" class="form-label">Username</label>
+            <input type="text" class="form-control" id="exampleInputUsername1" autocomplete="off" placeholder="Username">
           </div>
-
-
-
-
-        </div>
-
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" autocomplete="off" placeholder="Password">
+          </div>
+          <div class="form-check mb-3">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">
+              Remember me
+            </label>
+          </div>
+          <button type="submit" class="btn btn-primary me-2">Submit</button>
+          <button class="btn btn-secondary">Cancel</button>
+        </form>
 
       </div>
-
-      <!-- Footer Start -->
-      <?php include('_include/inc_footer.php'); ?>
-      <!-- Footer End -->
-
     </div>
   </div>
+  <div class="col-md-6 grid-margin stretch-card">
+    <div class="card">
+      <div class="card-body">
 
-  <!-- core:js -->
-  <script src="assets/vendors/core/core.js"></script>
-  <!-- endinject -->
+        <h6 class="card-title">Horizontal Form</h6>
 
-  <!-- Plugin js for this page -->
-  <script src="assets/vendors/flatpickr/flatpickr.min.js"></script>
-  <script src="assets/vendors/apexcharts/apexcharts.min.js"></script>
-  <!-- End plugin js for this page -->
+        <form class="forms-sample">
+          <div class="row mb-3">
+            <label for="exampleInputUsername2" class="col-sm-3 col-form-label">Username</label>
+            <div class="col-sm-9">
+              <input type="text" class="form-control" id="exampleInputUsername2" placeholder="Email">
+            </div>
+          </div>
+          <div class="row mb-3">
+            <label for="exampleInputEmail2" class="col-sm-3 col-form-label">Email</label>
+            <div class="col-sm-9">
+              <input type="email" class="form-control" id="exampleInputEmail2" autocomplete="off" placeholder="Email">
+            </div>
+          </div>
+          <div class="row mb-3">
+            <label for="exampleInputMobile" class="col-sm-3 col-form-label">Mobile</label>
+            <div class="col-sm-9">
+              <input type="number" class="form-control" id="exampleInputMobile" placeholder="Mobile number">
+            </div>
+          </div>
+          <div class="row mb-3">
+            <label for="exampleInputPassword2" class="col-sm-3 col-form-label">Password</label>
+            <div class="col-sm-9">
+              <input type="password" class="form-control" id="exampleInputPassword2" autocomplete="off" placeholder="Password">
+            </div>
+          </div>
+          <div class="form-check mb-3">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">
+              Remember me
+            </label>
+          </div>
+          <button type="submit" class="btn btn-primary me-2">Submit</button>
+          <button class="btn btn-secondary">Cancel</button>
+        </form>
 
-  <!-- inject:js -->
-  <script src="assets/vendors/feather-icons/feather.min.js"></script>
-  <script src="assets/js/template.js"></script>
-  <!-- endinject -->
+      </div>
+    </div>
+  </div>
+</div>
 
-  <!-- Custom js for this page -->
-  <script src="assets/js/dashboard-light.js"></script>
-  <!-- End custom js for this page -->
+                    <?php endforeach; ?>
+                </div>
+            <?php else : ?>
+                <p>No listings found.</p>
+            <?php endif; ?>
+            <!-- Footer Start -->
+            <?php include('_include/inc_footer.php'); ?>
+            <!-- Footer End -->
+
+        </div>
+    </div>
+
+    <!-- core:js -->
+    <script src="assets/vendors/core/core.js"></script>
+    <!-- endinject -->
+
+    <!-- Plugin js for this page -->
+    <script src="assets/vendors/flatpickr/flatpickr.min.js"></script>
+    <script src="assets/vendors/apexcharts/apexcharts.min.js"></script>
+    <!-- End plugin js for this page -->
+
+    <!-- inject:js -->
+    <script src="assets/vendors/feather-icons/feather.min.js"></script>
+    <script src="assets/js/template.js"></script>
+    <!-- endinject -->
+
+    <!-- Custom js for this page -->
+    <script src="assets/js/dashboard-light.js"></script>
+    <!-- End custom js for this page -->
 
 </body>
 
