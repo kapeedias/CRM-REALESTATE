7c65fc170a2e67d2fc7ad7af0528817652c8060c
Sai Deepak <sai@livewd.ca>
2023-12-05 05:40:42 +0530

    LL

--- _include/inc_sidemenu.php
+++ _include/inc_sidemenu.php
@@ -25,25 +25,7 @@
             <span class="link-title">View Inactive Listings</span>
         </a>
     </li>
-    <li class="nav-item nav-category">Library</li>
-    <li class="nav-item">
-        <a class="nav-link" data-bs-toggle="collapse" href="#uiComponents" role="button" aria-expanded="false" aria-controls="uiComponents">
-            <i class="link-icon" data-feather="feather"></i>
-            <span class="link-title">File Store</span>
-            <i class="link-arrow" data-feather="chevron-down"></i>
-        </a>
-        <div class="collapse" id="uiComponents">
-            <ul class="nav sub-menu">
-                <li class="nav-item">
-                    <a href="upload.php" class="nav-link">Upload</a>
-                </li>
-                <li class="nav-item">
-                    <a href="search.php" class="nav-link">Search</a>
-                </li>
-                
-            </ul>
-        </div>
-    </li>
+    
     <li class="nav-item">
         <a class="nav-link" data-bs-toggle="collapse" href="#advancedUI" role="button" aria-expanded="false" aria-controls="advancedUI">
             <i class="link-icon" data-feather="anchor"></i>
@@ -78,21 +60,7 @@
    
    
     <li class="nav-item nav-category">Content</li>
-    <li class="nav-item">
-        <a class="nav-link" data-bs-toggle="collapse" href="#general-pages" role="button" aria-expanded="false" aria-controls="general-pages">
-            <i class="link-icon" data-feather="book"></i>
-            <span class="link-title">Website Pages</span>
-            <i class="link-arrow" data-feather="chevron-down"></i>
-        </a>
-        <div class="collapse" id="general-pages">
-            <ul class="nav sub-menu">
-                <li class="nav-item">
-                    <a href="wp_contact.php" class="nav-link">Contact page</a>
-                </li>
-               
-            </ul>
-        </div>
-    </li>
+    
     <li class="nav-item nav-category"><a href="mailto:sai@livewd.ca">Help</a></li>
     
     
